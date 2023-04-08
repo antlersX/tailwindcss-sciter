2890be6ab83849872857a0a3f160e19595d0aef4
jzx <945627077@qq.com>
2023-04-08 11:38:20 +0800

    '删除jest'

--- jest/global-setup.js
+++
@@ -1,6 +0,0 @@
-let { execSync } = require('child_process')
-
-module.exports = function () {
-  execSync('npm run build:rust', { stdio: 'ignore' })
-  execSync('npm run generate:plugin-list', { stdio: 'ignore' })
-}
